f9f9a5de3d34c9ccb32666f8b5e7a710a8632f7b
Inés Van Gucht <inesvangucht@gmail.com>
2018-05-31 15:37:56 +0200

    voormiddag

--- src/Week2/FizzBuzz.java
+++ src/Week2/FizzBuzz.java
@@ -1,4 +1,34 @@
 package Week2;
 
 public class FizzBuzz {
+
+    public static void main(String[] args) {
+        // deelbaar door 3 is Fizz
+        // deelbaar door 5 is Buzz
+
+        int max = 30;
+
+        for (int teller = 1; teller <= max; teller++) {
+
+            if (teller % 15 == 0) {
+                System.out.println("FizzBuzz");
+
+            } else {
+                if (teller % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+                else if (teller % 3 == 15) {
+                    System.out.println("Fizz");
+                } else {
+                    System.out.println(teller);
+                }
+
+
+            }
+
+
+        }
+    }
 }
+
+
--- src/Week2/Wiskunde.java
+++ src/Week2/Wiskunde.java
@@ -1,11 +1,15 @@
-package Week2;
+/*package Week2;
 
 public class Wiskunde {
+
+    long getal = 0;
 
 
 
     /** bereken faculteit: bijvoorbeeld: "5! = 5*4*3*2*1 = 120" Input voorbeeld: "5", output "120"
      */
-    public static long faculteit(getal);
 
-}
+
+
+
+
