9d10f1043e05eeb4e398c9f480dc4503a6d9cef1
Inés Van gucht <inesvangucht@gmail.com>
2018-06-06 22:28:43 +0200

    game done

---
+++ src/Week3/Quickpick.java
@@ -0,0 +1,15 @@
+package Week3;
+
+public class Quickpick {
+    public static void main(String[] args) {
+
+        /**
+         * program kiest 6 willekeurige nummers
+         */
+
+        int nr = ;
+
+        System.out.println(Math.random());
+
+    }
+}
